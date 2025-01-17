25ec65b3f96799e81a7c271a7c6f83da09c5809d
Nikos Norman <nikos_norman87@hotmail.com>
2025-01-17 14:46:56 +0000

    Update User model with custom manager and superuser creation

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,31 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.conf import settings
 from django.core.validators import MinValueValidator
 from django.utils import timezone
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, **extra_fields):
+        if not username:
+            raise ValueError('The Username field must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, **extra_fields)
+        if password:
+            user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('user_type', 'landlord')
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(username, email, password, **extra_fields)
 
 class User(AbstractUser):
     USER_TYPES = (
@@ -10,6 +33,8 @@ class User(AbstractUser):
         ('landlord', 'Landlord'),
     )
     user_type = models.CharField(max_length=10, choices=USER_TYPES, null=True, blank=True)
+
+    objects = CustomUserManager()
 
     @property
     def is_landlord(self):
